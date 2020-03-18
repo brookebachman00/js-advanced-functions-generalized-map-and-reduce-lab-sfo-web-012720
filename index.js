56e2c35f0f9ba1b77484f029473817dd81338d3a
brookebachman00 <yourpetlover4411@yahoo.com>
2020-03-18 12:36:38 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Add your functions here
+function map(src, cb) {
+    let array = []
+  
+    for (let i = 0; i < src.length; i++) {
+      let theElement = src[i]
+      array.push(cb(theElement))
+    }
+  
+    return array;
+  }
+  
+  function reduce(src, cb, starting){
+    let array = (!!starting) ? starting : src[0]
+    let i = (!!starting) ? 0 : 1
+  
+    for (; i < src.length; i++) {
+      array = cb(src[i], array)
+    }
+  
+    return array;
+  }
+
+//map([1, 2, 3, -9], function(a){ return a * a }) //=> [1, 4, 9, 81]
